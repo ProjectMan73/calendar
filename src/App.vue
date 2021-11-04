65fcf86438b5f54e1d9bce540ede4ef9d825c9b5
Uvv-v <vlad.uto4kin@yandex.ru>
2021-11-05 01:32:40 +0400

    Add new send email component/fix

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,16 @@
 <template>
   <v-app>
     <AppCenter/>
-    <Sender/>
   </v-app>
 </template>
 
 <script>
 import AppCenter from './components/AppCenter';
-import Sender from "./components/Sender";
 
 export default {
   name: 'App',
 
   components: {
-    Sender,
       AppCenter: AppCenter,
   },
 
